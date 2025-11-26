0cc8ba9eef40cab5aec3e511ab463f7eb64bd885
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-11-26 17:22:31 +0100

    feat: add test users data for authentication flows

---
+++ data/users.ts
@@ -0,0 +1,14 @@
+export const standardUser = {
+    username: "standard_user",
+    password: "secret_sauce",
+};
+
+export const lockedOutUser = {
+    username: "locked_out_user",
+    password: "secret_sauce",
+};
+
+export const invalidUser = {
+    username: "invalid_user",
+    password: "wrong_password",
+};
