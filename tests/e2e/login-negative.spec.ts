e8736cfda9bb356e1d7f224760812c5b60bccdeb
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-11-30 20:58:35 +0100

    test: add negative login test scenarios

---
+++ tests/e2e/login-negative.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/LoginPage";
+import { lockedOutUser, invalidUser } from "../../data/users";
+
+test.describe("Login - negative scenarios", () => {
+
+    test("should show error for locked out user", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+
+        await loginPage.goto();
+        await loginPage.login(lockedOutUser.username, lockedOutUser.password);
+
+        await expect(loginPage.errorMessage).toBeVisible();
+    });
+
+    test("should show error for invalid credentials", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+
+        await loginPage.goto();
+        await loginPage.login(invalidUser.username, invalidUser.password);
+
+        await expect(loginPage.errorMessage).toBeVisible();
+    });
+
+});
